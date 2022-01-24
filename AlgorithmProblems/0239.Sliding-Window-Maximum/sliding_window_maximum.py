2a82b3e856ceff97296cd879c8315dc493f45293
junbaigit <89407070+junbaigit@users.noreply.github.com>
2022-01-24 14:17:05 -0500

    Update sliding_window_maximum.py

--- AlgorithmProblems/0239.Sliding-Window-Maximum/sliding_window_maximum.py
+++ AlgorithmProblems/0239.Sliding-Window-Maximum/sliding_window_maximum.py
@@ -1,11 +1,10 @@
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:        
         res = []                                         # list to store max of sliding window
         q = deque([])                                    # monostone queue used to keep largest value in top of queue
-
+        
         for i, n in enumerate(nums):
-            
+           
             while q and q[0] <= i - k:                   # keep queue in rang of k since queue[0] shoule be always <= i - k
                 q.popleft()
             
