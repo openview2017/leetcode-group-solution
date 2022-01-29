2220beb1fbde7b068f5c510a448da904a4b81ed0
Zhao Tang <zhao.tang@rice.edu>
2022-01-28 23:22:28 -0600

    1641

--- AlgorithmProblems/1026.Maximum-Difference-Between-Node-and-Ancestor/1641. Count Sorted Vowel Strings.java
+++ AlgorithmProblems/1026.Maximum-Difference-Between-Node-and-Ancestor/1641. Count Sorted Vowel Strings.java
@@ -1,4 +1,32 @@
+/*
+1641. Count Sorted Vowel Strings
+Medium
 
+Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: The 5 sorted strings that consist of vowels only are ["a","e","i","o","u"].
+Example 2:
+
+Input: n = 2
+Output: 15
+Explanation: The 15 sorted strings that consist of vowels only are
+["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
+Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+Example 3:
+
+Input: n = 33
+Output: 66045
+
+
+*/
 class Solution0 {
     // 定义dp[n][5]数组，其中dp[i][0-4]表示长度为i的以a-u结尾的字符串的个数，递推关系见代码。
     public int countVowelStrings(int n) {
