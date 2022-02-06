763ef8df577aa8fe7a1f0e03fe15278042129b41
Zhao Tang <zhao.tang@rice.edu>
2022-02-06 11:10:49 -0600

    0821

--- AlgorithmProblems/0821. Shortest Distance to a Character/main0821.java
+++ AlgorithmProblems/0821. Shortest Distance to a Character/main0821.java
@@ -44,6 +44,25 @@ class Solution0821 {
         }
         return res;
     }
+
+    public int[] shortestToChar2(String S, char C) { // two round scan, record the prev c location
+        int N = S.length();
+        int[] ans = new int[N];
+        int prev = Integer.MIN_VALUE / 2;
+
+        for (int i = 0; i < N; ++i) {
+            if (S.charAt(i) == C) prev = i;
+            ans[i] = i - prev;
+        }
+
+        prev = Integer.MAX_VALUE / 2;
+        for (int i = N-1; i >= 0; --i) {
+            if (S.charAt(i) == C) prev = i;
+            ans[i] = Math.min(ans[i], prev - i);
+        }
+
+        return ans;
+    }
 }
 
 /*abstract
