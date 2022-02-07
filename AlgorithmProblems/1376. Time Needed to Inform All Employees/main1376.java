4353f120c2750040500cd2066b47d106411e95b6
Zhao Tang <zhao.tang@rice.edu>
2022-02-07 13:57:08 -0600

    1376

--- AlgorithmProblems/1376. Time Needed to Inform All Employees/main1376.java
+++ AlgorithmProblems/1376. Time Needed to Inform All Employees/main1376.java
@@ -65,3 +65,18 @@ class Solution1376BFS {
         return ans;
     }
 }
+
+class Solution1376DFS0 {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) if (manager[i] != -1) graph[manager[i]].add(i);
+        return dfs(graph, headID, informTime);
+    }
+    private int dfs(List<Integer>[] graph, int u, int[] informTime) {
+        int ans = 0;
+        for (int v : graph[u])
+            ans = Math.max(ans, dfs(graph, v, informTime));
+        return ans + informTime[u];
+    }
+}
