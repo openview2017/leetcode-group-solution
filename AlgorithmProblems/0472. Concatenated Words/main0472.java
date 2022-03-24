321381482f654c6c346793207504842bdfb1141a
Zhao Tang <zhao.tang@rice.edu>
2022-03-24 17:14:49 -0500

    1610

--- AlgorithmProblems/0472. Concatenated Words/main0472.java
+++ AlgorithmProblems/0472. Concatenated Words/main0472.java
@@ -1,9 +1,40 @@
+import java.util.*;
+
+/*
+Example 1:
+
+Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+"dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
+
+Example 2:
+
+Input: words = ["cat","dog","catdog"]
+Output: ["catdog"]
+
+
+
+*/
+
+
 public class main0472 {
-    
+    public static void main(String[] args) {
+        Solution0472_0 sol = new Solution0472_0();
+        String[] words;
+        List<String> res;
+
+        words = new String[]{"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
+        System.out.println("Input : " + Arrays.deepToString(words));
+        res = sol.findAllConcatenatedWordsInADict(words);
+        System.out.println("Output: " + Arrays.deepToString(res.toArray()));
+    }
 }
 
 class Solution0472_0 { // tc:o(n3), sc:o(n)
-    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
         Set<String> preWords = new HashSet<>();
         Arrays.sort(words, new Comparator<String>() {
