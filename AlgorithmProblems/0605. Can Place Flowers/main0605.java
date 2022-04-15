3a74dc546f1a053b2147c61c0a988251387ad9ac
Zhao Tang <zhao.tang@rice.edu>
2022-04-15 02:20:31 -0500

    0605

--- AlgorithmProblems/0605. Can Place Flowers/main0605.java
+++ AlgorithmProblems/0605. Can Place Flowers/main0605.java
@@ -1,11 +1,23 @@
 public class main0605 {
     public static void main(String[] args) {
-        
+        Solution0605 sol = new Solution0605();
+        int flowerbed[], n;
+        boolean res;
+
+        flowerbed = new int[]{1,0,0,0,1};
+        n = 1;
+        res = sol.canPlaceFlowers(flowerbed, n);
+        System.out.println(res);
+
+        flowerbed = new int[]{1,0,0,0,1};
+        n = 2;
+        res = sol.canPlaceFlowers(flowerbed, n);
+        System.out.println(res);        
     }
 }
 
 
-class Solution {
+class Solution0605 {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 1;
         int result = 0;
