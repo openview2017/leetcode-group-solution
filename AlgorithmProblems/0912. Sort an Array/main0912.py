3b8b11dbbbed596a30e318da7a2a6877bcd4afc7
Zhao Tang <zhao.tang@rice.edu>
2022-04-16 23:13:22 -0500

    0912

--- AlgorithmProblems/0912. Sort an Array/main0912.py
+++ AlgorithmProblems/0912. Sort an Array/main0912.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 
 class MergeSort:
@@ -17,7 +18,58 @@ class MergeSort:
         merged.extend(right if right else left)
         return merged
 
+	# @bubbleSort, TLE
+    def bubbleSort(self, nums):
+        n = len(nums)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    
+	# @insertionSort, TLE
+    def insertionSort(self, nums): 
+        for i in range(1, len(nums)): 
+            key = nums[i]
+            j = i-1
+            while j >= 0 and key < nums[j] : 
+                    nums[j + 1] = nums[j] 
+                    j -= 1
+            nums[j + 1] = key
+    
+class QuickSort:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quicksort(nums, 0, len(nums)-1)
+        return nums
+    
+    def quicksort(self, nums, start, end):
+        if start >= end:
+            return
+        
+        part = self.part(nums, start, end) # partition the array
+        self.quicksort(nums, start, part-1)
+        self.quicksort(nums, part+1, end) # dont include the parition idx itself since left..... idx......right
+    
+    def part(self, nums, left, right):
+        idx = left
+        ran = randint(left, right)
+        nums[right], nums[ran] = nums[ran], nums[right]
+        pivot = nums[right]
+        
+        for i in range(left, right):
+            if nums[i] <= pivot:
+                nums[idx],nums[i] = nums[i], nums[idx]
+                idx+=1
+        
+        nums[idx],nums[right] = nums[right], nums[idx]
+        return idx # return the idx for part
+
 s = MergeSort()
+arr = [5,2,3,1]
+print(arr)
+sorted_arr = s.sortArray(arr)
+print(sorted_arr)
+
+s = QuickSort()
 arr = [5,2,3,1]
 print(arr)
 sorted_arr = s.sortArray(arr)
