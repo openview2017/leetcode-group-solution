a8d2dafe7fbe905ba612f5d9df474030bcc8bc28
liana3420 <67452380+liana3420@users.noreply.github.com>
2022-04-18 22:02:48 -0700

    change to main

--- AlgorithmProblems/0002. Add Two Numbers/Solution.java
+++ AlgorithmProblems/0002. Add Two Numbers/Solution.java
@@ -3,7 +3,7 @@
 虽然不难，但是总结几个点：=，容易忽略。
 while loop里面是||，会好做做很多
 判断l1和l2不为null，分别判断，分别往sum里加
-while loop的条件里还有sum!= 0，为||,这样，最后就算Listnode走完了，如果sum还么结束，就能再加一个node
+while loop的条件里还有sum!= 0，为||,这样，最后就算listnode走完了，如果sum还么结束，就能再加一个node
 
 */
 public class Solution {
