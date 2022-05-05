2d5dc6ad358a391ea10c2f6a30813a649c4f5b76
Zhao Tang <zhao.tang@rice.edu>
2022-05-04 19:45:33 -0500

    0057

--- AlgorithmProblems/1216.Valid-Palindrome-III/DFSplusMemo.java
+++ AlgorithmProblems/1216.Valid-Palindrome-III/DFSplusMemo.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1216 {
     
     public boolean isValidPalindrome(String s, int k) {
         int n = s.length();
@@ -34,4 +34,39 @@ class Solution {
         return cur;
     }
     
+}
+
+class Solution1216DP { // tc: o(N2), sc: o(n2) -> o(N)
+    public boolean isValidPalindrome(String s, int k) { // space optimization
+        int n = s.length();
+        int[][] memo = new int[2][n]; // memo [i, j] - remove min char
+        memo[n%2][n-1] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            memo[i%2][i] = 0;
+            for (int j = i+1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    memo[i%2][j] = memo[1-i%2][j-1];
+                } else {
+                    memo[i%2][j] = 1 + Math.min(memo[1-i%2][j], memo[i%2][j-1]);
+                }
+            }
+        }
+        return memo[0][n-1] <= k;
+    }    
+    // public boolean isValidPalindrome(String s, int k) {
+    //     int n = s.length();
+    //     int[][] memo = new int[n][n]; // memo [i, j] - remove min char
+    //     memo[n-1][n-1] = 0;
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         memo[i][i] = 0;
+    //         for (int j = i+1; j < n; j++) {
+    //             if (s.charAt(i) == s.charAt(j)) {
+    //                 memo[i][j] = memo[i+1][j-1];
+    //             } else {
+    //                 memo[i][j] = 1 + Math.min(memo[i+1][j], memo[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return memo[0][n-1] <= k;
+    // }
 }
