da2b9e7b1c0218f1d261d8eaf15d996b0e89c356
Zhao Tang <zhao.tang@rice.edu>
2022-05-05 22:39:26 -0500

    0437

--- AlgorithmProblems/0437. Path Sum III/main0437.java
+++ AlgorithmProblems/0437. Path Sum III/main0437.java
@@ -3,44 +3,28 @@ public class main0437 {
 }
 
 class Solution0437 {
-    int count = 0;
-    int k;
-    HashMap<Integer, Integer> h = new HashMap();
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        int[] count = new int[]{0};
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        helper(root, map, targetSum, 0, count);
+        return count[0];
+    }
     
-    public void preorder(TreeNode node, int currSum) {
-        if (node == null)
+    private void helper(TreeNode root, Map<Integer, Integer> map, int targetSum, int curSum, int[] count) {
+        if (root == null) {
             return;
+        }
         
-        // current prefix sum
-        currSum += node.val;
+        curSum += root.val;
+        count[0] += map.getOrDefault(curSum - targetSum, 0);
+        map.put(curSum, map.getOrDefault(curSum, 0) + 1);
+        helper(root.left, map, targetSum, curSum, count);
+        helper(root.right, map, targetSum, curSum, count);
+        map.put(curSum, map.get(curSum) - 1);
 
-        // here is the sum we're looking for
-        if (currSum == k)
-            count++;
-        
-        // number of times the curr_sum − k has occured already, 
-        // determines the number of times a path with sum k 
-        // has occured upto the current node
-        count += h.getOrDefault(currSum - k, 0);
-        
-        // add the current sum into hashmap
-        // to use it during the child nodes processing
-        h.put(currSum, h.getOrDefault(currSum, 0) + 1);
-
-        // process left subtree
-        preorder(node.left, currSum);
-        // process right subtree
-        preorder(node.right, currSum);
-
-        // remove the current sum from the hashmap
-        // in order not to use it during 
-        // the parallel subtree processing
-        h.put(currSum, h.get(currSum) - 1);
-    }    
-            
-    public int pathSum(TreeNode root, int sum) {
-        k = sum;
-        preorder(root, 0);
-        return count;
     }
 }
