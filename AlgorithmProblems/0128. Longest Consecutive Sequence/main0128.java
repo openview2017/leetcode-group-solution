2ce540f5e7c90343197c3772e5f03d80338a2a96
Zhao Tang <zhao.tang@rice.edu>
2022-05-06 05:37:20 -0500

    128

---
+++ AlgorithmProblems/0128. Longest Consecutive Sequence/main0128.java
@@ -0,0 +1,32 @@
+public class main0128 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution0128 {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
