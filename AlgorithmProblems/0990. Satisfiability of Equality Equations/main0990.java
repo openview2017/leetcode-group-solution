7bc04c3a3070e78309c543523d46682cf6a3848d
Zhao Tang <zhao.tang@rice.edu>
2022-05-06 05:26:03 -0500

    990

--- AlgorithmProblems/0990. Satisfiability of Equality Equations/main0990.java
+++ AlgorithmProblems/0990. Satisfiability of Equality Equations/main0990.java
@@ -7,6 +7,10 @@ public class main0990 {
         equations = new String[]{"a==b","b!=a"};
         res = sol.equationsPossible(equations);
         System.out.println(res);
+ 
+        equations = new String[]{"a==b","b==a"};
+        res = sol.equationsPossible(equations);
+        System.out.println(res);
     }
 }
 
@@ -47,5 +51,67 @@ class Solution0990 { // 17 - 22 - 23, tc/sc: o(N)
     
     private void merge(int[] p, int i1, int i2) {
         p[find(p, i1)] = find(p, i2);
+    }
+}
+
+class Solution0990BFS { // 58 - 09
+    public boolean equationsPossible(String[] equations) {
+        int[][] eq = new int[26][26]; // 0-not, 1-equal
+        for (int i = 0; i < 26; i++) {
+            eq[i][i] = 1;
+        }
+        // process equals
+        for (String s : equations) {
+            
+            if (s.charAt(1) == '!') {
+                if (s.charAt(0) == s.charAt(3)) {
+                    return false;
+                }   
+                continue;
+            }
+            //System.out.println(1);
+            int idx1 = s.charAt(0) - 'a';            
+            int idx2 = s.charAt(3) - 'a';
+            eq[idx1][idx2] = 1;            
+            eq[idx2][idx1] = 1;
+        }
+        // process not equal
+        for (String s : equations) {
+            if (s.charAt(1) == '=') {
+                // if (eq[s.charAt(0)-'a'][s.charAt(3)-'a'] == 0) {
+                //     return false;
+                // }   
+                continue;
+            }
+            //System.out.println(2);
+            int idx1 = s.charAt(0) - 'a';            
+            int idx2 = s.charAt(3) - 'a';
+            //System.out.println(bfs(eq, idx1, idx2));
+            if (bfs(eq, idx1, idx2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean bfs(int[][] eq, int idx1, int idx2) { // check equal
+        boolean[] visited = new boolean[26];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offerLast(idx1);
+        visited[idx1] = true;
+        while (!queue.isEmpty()) {
+            int tmp = queue.pollFirst();
+            if (eq[tmp][idx2] == 1 || eq[idx2][tmp] == 1) {
+                return true;
+            }            
+            for (int i = 0; i < 26; i++) {
+                if (visited[i]) continue;
+                if (eq[tmp][i] == 1) {
+                    queue.offerLast(i);
+                    visited[i] = true;
+                }
+            }
+        }
+        return false;
     }
 }
