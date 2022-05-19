73376414b30c9938d807d36b1723dea188b4057b
Zhao Tang <zhao.tang@rice.edu>
2022-05-19 07:34:33 -0500

    1160

--- AlgorithmProblems/1160. Find Words That Can Be Formed by Characters/main1160.java
+++ AlgorithmProblems/1160. Find Words That Can Be Formed by Characters/main1160.java
@@ -1,9 +1,18 @@
 public class main1160 {
-    
+    public static void main(String[] args) {
+        Solution1160 sol = new Solution1160();
+        String words[], chars;
+        int count;
+
+        words = new String[]{"cat","bt","hat","tree"};
+        chars = "atach";
+        count = sol.countCharacters(words, chars);
+        System.out.println(count);
+    }
 }
 
 
-class Solution1160 { // 27 - 31
+class Solution1160 { // 27 - 31, tc: o(n), sc: o(1)
     public int countCharacters(String[] words, String chars) {
         int[] charDict = new int[26];
         int count = 0;
