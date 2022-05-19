cfaf154ad5b00685c7ead7c540b1aa59cb16fbe6
Zhao Tang <zhao.tang@rice.edu>
2022-05-19 08:46:55 -0500

    1365

--- AlgorithmProblems/1365. How Many Numbers Are Smaller Than the Current Number/main1365.java
+++ AlgorithmProblems/1365. How Many Numbers Are Smaller Than the Current Number/main1365.java
@@ -24,3 +24,20 @@ class Solution1365 { // 23
     }
 }// tc/sc: o(N)
 // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/discuss/524996/JAVA-beats-100-O(n)
+
+
+class Solution1365_2 { // 38 - 40
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        Map<Integer, Integer> rank = new HashMap<>();
+        int[] arr = nums.clone();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            rank.putIfAbsent(arr[i], i); // put doesn't work for duplicate case
+        }
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = rank.get(nums[i]);
+        }
+        return res;
+    }
+} // tc: o(nlogn), sc: o(n)
