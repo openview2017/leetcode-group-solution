dd36d4f639426628e64215978df8ee0708cee7f0
Zhao Tang <zhao.tang@rice.edu>
2022-05-19 08:33:31 -0500

    1365

---
+++ AlgorithmProblems/1365. How Many Numbers Are Smaller Than the Current Number/main1365.java
@@ -0,0 +1,25 @@
+public class main1365 {
+    
+}
+
+
+class Solution1365 { // 23
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101]; // count[i] # appearance <= i
+        for (int num : nums) {
+            count[num]++;
+        }
+        for (int i = 1; i <= 100; i++) {
+            count[i] += count[i-1];
+        }
+        int[] res = new int[nums.length];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == 0) { //!!!!!!!!!!!!
+                res[j] = 0;
+            } else {
+                res[j] = count[nums[j]- 1] ; // !!!!!!!!!!!
+            }
+        }
+        return res;
+    }
+}// tc/sc: o(N)
