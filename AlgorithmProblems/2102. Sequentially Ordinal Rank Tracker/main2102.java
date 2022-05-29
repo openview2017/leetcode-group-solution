19f1f0a37ab355b7c67560a66e7f6585d2d52752
Zhao Tang <zhao.tang@rice.edu>
2022-05-29 18:49:28 -0500

    2102

--- AlgorithmProblems/2102. Sequentially Ordinal Rank Tracker/main2102.java
+++ AlgorithmProblems/2102. Sequentially Ordinal Rank Tracker/main2102.java
@@ -1,5 +1,28 @@
 public class main2102 {
-    
+    public static void main(String[] args) {
+        SORTracker tracker = new SORTracker(); // Initialize the tracker system.
+        tracker.add("bradford", 2); // Add location with name="bradford" and score=2 to the system.
+        tracker.add("branford", 3); // Add location with name="branford" and score=3 to the system.
+        System.out.println(tracker.get());              // The sorted locations, from best to worst, are: branford, bradford.
+                                    // Note that branford precedes bradford due to its higher score (3 > 2).
+                                    // This is the 1st time get() is called, so return the best location: "branford".
+        tracker.add("alps", 2);     // Add location with name="alps" and score=2 to the system.
+        System.out.println(tracker.get());              // Sorted locations: branford, alps, bradford.
+                                    // Note that alps precedes bradford even though they have the same score (2).
+                                    // This is because "alps" is lexicographically smaller than "bradford".
+                                    // Return the 2nd best location "alps", as it is the 2nd time get() is called.
+        tracker.add("orland", 2);   // Add location with name="orland" and score=2 to the system.
+        System.out.println(tracker.get());              // Sorted locations: branford, alps, bradford, orland.
+                                    // Return "bradford", as it is the 3rd time get() is called.
+        tracker.add("orlando", 3);  // Add location with name="orlando" and score=3 to the system.
+        System.out.println(tracker.get());              // Sorted locations: branford, orlando, alps, bradford, orland.
+                                    // Return "bradford".
+        tracker.add("alpine", 2);   // Add location with name="alpine" and score=2 to the system.
+        System.out.println(tracker.get());              // Sorted locations: branford, orlando, alpine, alps, bradford, orland.
+                                    // Return "bradford".
+        System.out.println(tracker.get());              // Sorted locations: branford, orlando, alpine, alps, bradford, orland.
+                                    // Return "orland".
+    }
 }
 
 class SORTracker {
