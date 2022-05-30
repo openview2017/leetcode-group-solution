4ce2c456665a7db1062a3a6d46c674faf7dd2c8c
Zhao Tang <zhao.tang@rice.edu>
2022-05-29 19:31:13 -0500

    2104

--- AlgorithmProblems/2104.Sum of Subarray Ranges/main2104.java
+++ AlgorithmProblems/2104.Sum of Subarray Ranges/main2104.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.function.BiPredicate;
+//import java.util.function.BiPredicate;
 
 public class main2104 {
     public static void main(String[] args) {
@@ -10,23 +10,56 @@ public class main2104 {
     }
 }
 
-
-class Solution2104 { // 08
+class Solution2104 { // 25-30
     public long subArrayRanges(int[] nums) {
-        return getExSums(nums, (i1,i2)->(i1>i2)) - getExSums(nums, (i1,i2)->(i2>i1));
+        return getMaxSum(nums) - getMinSum(nums);
     }
     
-    private long getExSums(int[] nums, BiPredicate<Integer, Integer> comp) {
+    private long getMinSum(int[] nums) {
         Deque<Integer> stack = new ArrayDeque<>();
-        long res = 0;
+        long sums = 0;
         for (int i = 0; i <= nums.length; i++) {
-            while (!stack.isEmpty() && (i == nums.length || comp.test(nums[i], nums[stack.peekLast()]))) { //comp.test !!!
-                int pivot = stack.pollLast(), val = nums[pivot], left = stack.isEmpty() ? -1 : stack.peekLast();
-                res += (long)val * (i - pivot) * (pivot - left);
+            while (!stack.isEmpty() && (i == nums.length || nums[stack.peekLast()] >= nums[i])) {
+                int pivot = stack.pollLast(), val = nums[pivot];
+                int left = stack.isEmpty() ? -1 : stack.peekLast();
+                sums += (long) (i - pivot) * (pivot - left) * val;
             }
             stack.offerLast(i);
-        }
-        return res;
+        } 
+        return sums;
+    }
+    
+    private long getMaxSum(int[] nums) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        long sums = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            while (!stack.isEmpty() && (i == nums.length || nums[stack.peekLast()] <= nums[i])) {
+                int pivot = stack.pollLast(), val = nums[pivot];
+                int left = stack.isEmpty() ? -1 : stack.peekLast();
+                sums += (long) (i - pivot) * (pivot - left) * val;
+            }
+            stack.offerLast(i);
+        } 
+        return sums;        
     }
 }
+
+// class Solution2104 { // 08
+//     public long subArrayRanges(int[] nums) {
+//         return getExSums(nums, (i1,i2)->(i1>i2)) - getExSums(nums, (i1,i2)->(i2>i1));
+//     }
+    
+//     private long getExSums(int[] nums, BiPredicate<Integer, Integer> comp) {
+//         Deque<Integer> stack = new ArrayDeque<>();
+//         long res = 0;
+//         for (int i = 0; i <= nums.length; i++) {
+//             while (!stack.isEmpty() && (i == nums.length || comp.test(nums[i], nums[stack.peekLast()]))) { //comp.test !!!
+//                 int pivot = stack.pollLast(), val = nums[pivot], left = stack.isEmpty() ? -1 : stack.peekLast();
+//                 res += (long)val * (i - pivot) * (pivot - left);
+//             }
+//             stack.offerLast(i);
+//         }
+//         return res;
+//     }
+// }
 // https://leetcode.com/problems/sum-of-subarray-ranges/discuss/1624268/Reformulate-Problem-O(n)
