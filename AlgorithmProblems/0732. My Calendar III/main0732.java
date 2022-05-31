d7bffe1d8f10efa542438f3cf20154af816753ca
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 02:08:42 -0500

    0731

--- AlgorithmProblems/0732. My Calendar III/main0732.java
+++ AlgorithmProblems/0732. My Calendar III/main0732.java
@@ -1,12 +1,14 @@
+import java.util.TreeMap;
+
 public class main0732 {
     public static void main(String[] args) {
         MyCalendarThree myCalendarThree = new MyCalendarThree();
-        myCalendarThree.book(10, 20); // return 1, The first event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
-        myCalendarThree.book(50, 60); // return 1, The second event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
-        myCalendarThree.book(10, 40); // return 2, The third event [10, 40) intersects the first event, and the maximum k-booking is a 2-booking.
-        myCalendarThree.book(5, 15); // return 3, The remaining events cause the maximum K-booking to be only a 3-booking.
-        myCalendarThree.book(5, 10); // return 3
-        myCalendarThree.book(25, 55); // return 3
+        System.out.println(myCalendarThree.book(10, 20)); // return 1, The first event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+        System.out.println(myCalendarThree.book(50, 60)); // return 1, The second event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+        System.out.println(myCalendarThree.book(10, 40)); // return 2, The third event [10, 40) intersects the first event, and the maximum k-booking is a 2-booking.
+        System.out.println(myCalendarThree.book(5, 15)); // return 3, The remaining events cause the maximum K-booking to be only a 3-booking.
+        System.out.println(myCalendarThree.book(5, 10)); // return 3
+        System.out.println(myCalendarThree.book(25, 55)); // return 3
     }
 }
 
@@ -15,7 +17,7 @@ class MyCalendarThree {
     TreeMap<Integer, Integer> delta;
 
     public MyCalendarThree() {
-        delta = new TreeMap();
+        delta = new TreeMap<>();
     }
 
     public int book(int start, int end) {
