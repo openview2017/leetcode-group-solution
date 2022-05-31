24975f56147e7cc951370d91d882ed69d252ad8c
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 01:37:36 -0500

    0029

--- AlgorithmProblems/0895. Maximum Frequency Stack/main0895.java
+++ AlgorithmProblems/0895. Maximum Frequency Stack/main0895.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class main0895 {
     public static void main(String[] args) {
         FreqStack freqStack = new FreqStack();
@@ -7,10 +9,10 @@ public class main0895 {
         freqStack.push(7); // The stack is [5,7,5,7]
         freqStack.push(4); // The stack is [5,7,5,7,4]
         freqStack.push(5); // The stack is [5,7,5,7,4,5]
-        freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
-        freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
-        freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
-        freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+        System.out.println(freqStack.pop());   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+        System.out.println(freqStack.pop());   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+        System.out.println(freqStack.pop());   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+        System.out.println(freqStack.pop());   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
     }
 }
 
