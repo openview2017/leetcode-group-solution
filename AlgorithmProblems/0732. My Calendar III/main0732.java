92c13d3bc8fd93daafdcdb3e753ebd70b281bfab
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 02:07:34 -0500

    0731

--- AlgorithmProblems/0732. My Calendar III/main0732.java
+++ AlgorithmProblems/0732. My Calendar III/main0732.java
@@ -1,5 +1,13 @@
 public class main0732 {
-    
+    public static void main(String[] args) {
+        MyCalendarThree myCalendarThree = new MyCalendarThree();
+        myCalendarThree.book(10, 20); // return 1, The first event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+        myCalendarThree.book(50, 60); // return 1, The second event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+        myCalendarThree.book(10, 40); // return 2, The third event [10, 40) intersects the first event, and the maximum k-booking is a 2-booking.
+        myCalendarThree.book(5, 15); // return 3, The remaining events cause the maximum K-booking to be only a 3-booking.
+        myCalendarThree.book(5, 10); // return 3
+        myCalendarThree.book(25, 55); // return 3
+    }
 }
 
 
@@ -24,11 +32,7 @@ class MyCalendarThree {
 }
 
 /*abstract
-Time Complexity: O(N^2)O(N 
-2
- ), where NN is the number of events booked. For each new event, we traverse delta in O(N)O(N) time. In Python, this is O(N^2 \log N)O(N 
-2
- logN) owing to the extra sort step.
+Time Complexity: O(N^2)O(N^2), where NN is the number of events booked. For each new event, we traverse delta in O(N)O(N) time. In Python, this is O(N^2 \log N)O(N^2logN) owing to the extra sort step.
 
 Space Complexity: O(N)O(N), the size of delta
 */
