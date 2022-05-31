46f2e806e73324427579864ed5013cce82df20e7
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 02:23:59 -0500

    0723

--- AlgorithmProblems/0723. Candy Crush/main0723.java
+++ AlgorithmProblems/0723. Candy Crush/main0723.java
@@ -1,11 +1,20 @@
 public class main0723 {
     public static void main(String[] args) {
-        
+        Solution0723 sol = new Solution0723();
+        int[][] board = {{110,5,112,113,114},{210,211,5,213,214},{310,311,3,313,314},{410,411,412,5,414},{5,1,512,3,3},{610,4,1,613,614},{710,1,2,713,714},{810,1,2,1,1},{1,1,2,2,2},{4,1,4,4,1014}};
+        for (int[] b : board) {
+            System.out.println(Arrays.toString(b));
+        }
+        System.out.println();
+        res = sol.candyCrush(board);
+        for (int[] b : res) {
+            System.out.println(Arrays.toString(b));
+        }       
     }
 }
 
 
-class Solution {
+class Solution0723 {
     public int[][] candyCrush(int[][] board) {
         int R = board.length, C = board[0].length;
         boolean todo = false;
