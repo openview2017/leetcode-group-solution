6fcdd3386d8b6ebce92981cc10c45a2d338213a9
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 02:30:43 -0500

    0723

--- AlgorithmProblems/0723. Candy Crush/main0723.java
+++ AlgorithmProblems/0723. Candy Crush/main0723.java
@@ -1,15 +1,25 @@
+import java.util.*;
+
 public class main0723 {
     public static void main(String[] args) {
         Solution0723 sol = new Solution0723();
         int[][] board = {{110,5,112,113,114},{210,211,5,213,214},{310,311,3,313,314},{410,411,412,5,414},{5,1,512,3,3},{610,4,1,613,614},{710,1,2,713,714},{810,1,2,1,1},{1,1,2,2,2},{4,1,4,4,1014}};
         for (int[] b : board) {
-            System.out.println(Arrays.toString(b));
+            System.out.print("[");
+            for (int x : b) {
+                System.out.printf("%4d,", x);
+            }
+            System.out.println("]");
         }
         System.out.println();
-        res = sol.candyCrush(board);
+        int[][] res = sol.candyCrush(board);
         for (int[] b : res) {
-            System.out.println(Arrays.toString(b));
-        }       
+            System.out.print("[");
+            for (int x : b) {
+                System.out.printf("%4d,", x);
+            }
+            System.out.println("]");
+        }    
     }
 }
 
