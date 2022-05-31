9f7097b1c3737d54c2641940bdb7e5c9b658e47e
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 17:36:01 -0500

    456

---
+++ AlgorithmProblems/0456. 132 Pattern/main0456.java
@@ -0,0 +1,22 @@
+public class main0456 {
+    
+}
+
+class Solution0456 {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> d = new ArrayDeque<>();
+        int k = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < k) return true;
+            while (!d.isEmpty() && d.peekLast() < nums[i]) {
+                // 事实上，k 的变化也具有单调性，直接使用 k = pollLast() 也是可以的
+                k = Math.max(k, d.pollLast()); 
+            }
+            d.addLast(nums[i]);
+        }
+        return false;
+    }
+}
+
+//https://leetcode.cn/problems/132-pattern/solution/xiang-xin-ke-xue-xi-lie-xiang-jie-wei-he-95gt/
