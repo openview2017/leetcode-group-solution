230acc75e73bcf4d036fdb014ec54c40dfb04143
Zhao Tang <zhao.tang@rice.edu>
2022-05-31 02:06:25 -0500

    0731

--- AlgorithmProblems/0731. My Calendar II/main0731.java
+++ AlgorithmProblems/0731. My Calendar II/main0731.java
@@ -1,12 +1,14 @@
+import java.util.TreeMap;
+
 public class main0731 {
     public static void main(String[] args) {
         MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
-        myCalendarTwo.book(10, 20); // return True, The event can be booked. 
-        myCalendarTwo.book(50, 60); // return True, The event can be booked. 
-        myCalendarTwo.book(10, 40); // return True, The event can be double booked. 
-        myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a triple booking.
-        myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is already double booked.
-        myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55) will be double booked with the second event.        
+        System.out.println(myCalendarTwo.book(10, 20)); // return True, The event can be booked. 
+        System.out.println(myCalendarTwo.book(50, 60)); // return True, The event can be booked. 
+        System.out.println(myCalendarTwo.book(10, 40)); // return True, The event can be double booked. 
+        System.out.println(myCalendarTwo.book(5, 15));  // return False, The event cannot be booked, because it would result in a triple booking.
+        System.out.println(myCalendarTwo.book(5, 10)); // return True, The event can be booked, as it does not use time 10 which is already double booked.
+        System.out.println(myCalendarTwo.book(25, 55)); // return True, The event can be booked, as the time in [25, 40) will be double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55) will be double booked with the second event.        
     }
 }
 
