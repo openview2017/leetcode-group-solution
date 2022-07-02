8293acfc471fc2ce05e56d440d1679c337e75899
Zhao Tang <zhao.tang@rice.edu>
2022-07-02 03:44:27 -0500

    0029

--- AlgorithmProblems/0029. Divide Two Integers/29. Divide-Two-Integers.py
+++ AlgorithmProblems/0029. Divide Two Integers/29. Divide-Two-Integers.py
@@ -35,3 +35,17 @@ class Solution:
         if result < -1<<31:
             result = -1<<31
         return result
+
+class Solution0029:
+    def divide(self, dividend: int, divisor: int) -> int:
+        MIN_VAL, MAX_VAL = -2**31, 2**31-1
+        if (dividend == MIN_VAL and divisor == -1):
+            return MAX_VAL
+        div1, div2 = abs(dividend), abs(divisor)
+        res = 0
+        for i in range(31,-1,-1):
+            if (div1 >> i) >= div2:
+                res += (1 << i)
+                div1 -= (div2 << i)
+        
+        return res if (dividend > 0) == (divisor > 0) else -res
